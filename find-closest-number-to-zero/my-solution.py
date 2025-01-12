f3af0ccc3d8019f8d3dea9e36d278c73734b76d3
Akmal Ali Jasmin <jasminaaa.20@uom.lk>
2025-01-12 11:55:19 -0800

    Create my-solution.py

---
+++ find-closest-number-to-zero/my-solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findClosestNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        closest_number = 100001
+
+        for num in nums:
+            if abs(num) < abs(closest_number) or (abs(num) == abs(closest_number) and num > closest_number):
+                closest_number = num
+
+        return closest_number
