a2e67b37546f1d3b4d3423ee912f93e5ffbb2994
Akmal Ali Jasmin <jasminaaa.20@uom.lk>
2025-01-12 12:01:55 -0800

    Update video-solution.py

--- find-closest-number-to-zero/video-solution.py
+++ find-closest-number-to-zero/video-solution.py
@@ -1,11 +1,11 @@
 class Solution:
-  def findClosestNumber(self, nums: List[int]) -> int:
-    closest = nums[Q]
-    for x in nums:
-      if abs(x) < abs(closest):
-        closest = x
+    def findClosestNumber(self, nums: List[int]) -> int:
+        closest = nums[Q]
+        for x in nums:
+            if abs(x) < abs(closest):
+                closest = x
 
-    if closest < @ and abs(closest) in nums:
-      return abs(closest)
-    else:
-      return closest
+        if closest < @ and abs(closest) in nums:
+            return abs(closest)
+        else:
+            return closest
