ff10681bbcedc53a5b7da45c8711824acf41d092
Akmal Ali Jasmin <jasminaaa.20@uom.lk>
2025-02-13 11:52:25 -0800

    Add time and space complexity

--- roman-to-integer/my-solution.py
+++ roman-to-integer/my-solution.py
@@ -7,15 +7,20 @@ class Solution(object):
         symbol_values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
         sum = symbol_values[s[-1]]
-        length = len(s)
-        i = length - 2
 
-        while i >= 0:
-            if symbol_values[s[i]] < symbol_values[s[i+1]]:
-                sum -= symbol_values[s[i]]
+        index = len(s) - 2
+
+        while index >= 0:
+            current_value = symbol_values[s[index]]
+            proceeding_value = symbol_values[s[index+1]]
+
+            if current_value < proceeding_value:
+                sum -= current_value
             else:
-                sum += symbol_values[s[i]]
-            i -= 1
+                sum+= current_value
+            index -= 1
         
         return sum
-            
+        # Time complexity: O(n)
+        # Space complexity: O(1)
+        
